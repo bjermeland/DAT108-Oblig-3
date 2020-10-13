7583ccb74cc229894de996e3bb2bccb5deaf66a7
h584979 <markus.bjermeland@hotmail.com>
2020-10-13 19:04:27 +0200

    Added Oppgave 2 :zap:

---
+++ Oppgave 2/Dice.js
@@ -0,0 +1,15 @@
+class Dice {
+    constructor(value) {
+        this.value = value
+    }
+
+    roll() {
+        return (this.value = Math.floor(Math.random() * 6 + 1))
+    }
+
+    getValue() {
+        return this.value
+    }
+}
+
+export default Dice
